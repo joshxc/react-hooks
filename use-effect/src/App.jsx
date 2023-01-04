e5af233b42fdcf5036b623177d5da3e2279b2be9
Josh Taylor <josht@duck.com>
2023-01-04 10:12:40 +1000

    add IntervalClassCounter class component

--- use-effect/src/App.jsx
+++ use-effect/src/App.jsx
@@ -1,8 +1,9 @@
-import ClassCounter from './components/ClassCounter';
-import HookCounter from './components/HookCounter';
-import ClassMouse from './components/ClassMouse';
+// import ClassCounter from './components/ClassCounter';
+// import HookCounter from './components/HookCounter';
+// import ClassMouse from './components/ClassMouse';
 // import HookMouse from './components/HookMouse';
-import MouseContainer from './components/MouseContainer';
+// import MouseContainer from './components/MouseContainer';
+import IntervalClassCounter from './components/IntervalClassCounter';
 import './App.css';
 
 function App() {
@@ -12,7 +13,8 @@ function App() {
       {/* <HookCounter /> */}
       {/* <ClassMouse /> */}
       {/* <HookMouse /> */}
-      <MouseContainer />
+      {/* <MouseContainer /> */}
+      <IntervalClassCounter />
     </>
   );
 }
