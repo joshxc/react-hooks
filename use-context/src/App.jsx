c010442df9fc87e94e51496aeb7479323ff80d70
Josh Taylor <josht@duck.com>
2023-01-06 07:45:00 +1000

    complete the old way of providing context to nested components

--- use-context/src/App.jsx
+++ use-context/src/App.jsx
@@ -1,11 +1,18 @@
+import React from 'react';
 import ComponentA from './components/ComponentA';
 import './App.css';
+
+export const UserContext = React.createContext();
+export const UserContext2 = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      {/* Data passed as props through to ComponentC */}
-      <ComponentA />
+      <UserContext.Provider value={'123'}>
+        <UserContext2.Provider value={'abc'}>
+          <ComponentA />
+        </UserContext2.Provider>
+      </UserContext.Provider>
     </div>
   );
 }
--- use-context/src/components/ComponentC.jsx
+++ use-context/src/components/ComponentC.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { UserContext, UserContext2 } from '../App';
 
 function ComponentC() {
   return (
-    <div>ComponentC</div>
-  )
+    <div>
+      <UserContext.Consumer>
+        {(value1) => {
+          return (
+            <UserContext2.Consumer>
+              {(value2) => {
+                return (
+                  <div>
+                    <p>UserContext value: {value1}</p>
+                    <p>UserContext2 value: {value2}</p>
+                  </div>
+                );
+              }}
+            </UserContext2.Consumer>
+          );
+        }}
+      </UserContext.Consumer>
+    </div>
+  );
 }
 
-export default ComponentC
+export default ComponentC;
