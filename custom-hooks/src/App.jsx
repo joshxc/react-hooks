a59a4e89aec84f460c65d52577376c05a3caef41
Josh Taylor <josht@duck.com>
2023-01-07 17:25:21 +1000

    add custom-hooks dir and useDocumentTitle custom hook

---
+++ custom-hooks/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import DocTitle from './components/DocTitle';
+import DocTitle2 from './components/DocTitle2';
+
+function App() {
+  return (
+    <div className="App">
+      <DocTitle />
+      <DocTitle2 />
+    </div>
+  );
+}
+
+export default App;
